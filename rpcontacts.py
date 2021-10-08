5065240bf95ca8c50a00f70c960dfe8265c4f585
ajandaur <anmol.jandaur@gmail.com>
2021-10-07 23:43:53 -0700

    database driver is missing

---
+++ rpcontacts.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# contact_book/rpcontacts.py
+
+"""This module provides RP Contacts entry point script."""
+
+from rpcontacts.main import main
+
+if __name__ == "__main__":
+    main()
+
